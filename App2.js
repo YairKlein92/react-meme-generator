82890638db4ef3231ddd6896385c4f321098fbb8
Csaba Klein <sympla@gmail.com>
2023-01-19 21:51:36 +0100

    Fix fetch API

---
+++ App2.js
@@ -0,0 +1,102 @@
+// import './App.css';
+// import { saveAs } from 'file-saver';
+// import React, { useEffect, useState } from 'react';
+
+// export default function App() {
+//   function containsNumbers(str) {
+//     return /\d/.test(str);
+//   }
+
+//   // let memeList = '';
+//   // const links = [];
+//   // let i = 1;
+//   // for (const api of jsonData) {
+//   //   memeList += i + ', ' + JSON.stringify(api.id) + '  ';
+//   //   links.push(JSON.stringify(api.blank));
+//   //   i++;
+//   // }
+
+//   // let linkArray = [];
+
+//   // linkArray = links.map((link) => {
+//   //   return link.slice(1, link.length - 5);
+//   // });
+//   // console.log(linkArray);
+//   const link =
+//     'https://api.memegen.link/images/buzz/memes/memes_everywhere.gif';
+//   const [chosenMeme, setChosenMeme] = useState('');
+//   const [top, setTop] = useState('');
+//   const [bottom, setBottom] = useState('');
+//   const [linkArray, setLinkArray] = useState([]);
+//   const memeList = [];
+//   // const downloadLink =
+//   //   chosenMeme === ''
+//   //     ? link
+//   //     : `${linkArray[chosenMeme - 1]}/${top === '' ? '_' : top}${
+//   //         bottom === '' ? bottom : '/' + bottom
+//   //       }.png`;'https://api.memegen.link/templates'
+//   async function componentDidMount() {
+//     // GET request using fetch with async/await
+//     const response = await fetch('https://api.memegen.link/templates');
+//     const data = await response.json();
+//     data.map((resp) => {
+//       return memeList.push(resp.id);
+//     });
+//     this.setState({ totalReactPackages: data.total });
+//   }
+//   componentDidMount();
+//   console.log(memeList);
+//   return (
+//     <>
+//       <label htmlFor="top">Top text:</label>
+//       <input
+//         id="top"
+//         onChange={(event) => {
+//           setTop(top === '' ? '__' : event.target.value);
+//         }}
+//       />
+//       <label htmlFor="bottom">Bottom text:</label>
+//       <input
+//         id="bottom"
+//         onChange={(event) => {
+//           setBottom(event.target.value);
+//         }}
+//       />
+//       <label htmlFor="meme">Choose a meme by its number </label>
+//       <input
+//         id="meme"
+//         onChange={(event) => {
+//           setChosenMeme(event.target.value);
+//           console.log(linkArray[chosenMeme - 5]);
+//         }}
+//       />
+//       <br />
+//       Download
+//       {JSON.stringify(linkArray)}
+//       <button
+//         onClick={() =>
+//           saveAs(
+//             chosenMeme === ''
+//               ? link
+//               : `${linkArray[chosenMeme - 1]}/${top === '' ? '_' : top}${
+//                   bottom === '' ? bottom : '/' + bottom
+//                 }.png`,
+//             'meme.jpg',
+//           )
+//         }
+//       >
+//         Download
+//       </button>
+//       <img
+//         data-test-id="meme-image"
+//         style={{ width: '400px', height: '400px' }}
+//         alt="meme"
+//         src={memeLi}
+//       />
+//       <br /> <br /> <br /> <br /> <br /> <br />
+//       memes you can choose from: <br />
+//       {memeList}
+//       {/* data-test-id="meme-image" */}
+//     </>
+//   );
+// }
--- src/App.js
+++ src/App.js
@@ -3,46 +3,28 @@ import { saveAs } from 'file-saver';
 import React, { useState } from 'react';
 
 export default function App() {
-  function httpGet(theUrl) {
-    const xmlHttp = new XMLHttpRequest();
-    xmlHttp.open('GET', theUrl, false); // false for synchronous request
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-  }
-
-  function containsNumbers(str) {
-    return /\d/.test(str);
-  }
-  const htmlString = httpGet('https://api.memegen.link/templates');
-  const jsonData = JSON.parse(htmlString);
-  console.log(jsonData);
-  let memeList = '';
-  const links = [];
-  let i = 1;
-  for (const api of jsonData) {
-    memeList += i + ', ' + JSON.stringify(api.id) + '  ';
-    links.push(JSON.stringify(api.blank));
-    i++;
-  }
-
-  let linkArray = [];
-
-  linkArray = links.map((link) => {
-    return link.slice(1, link.length - 5);
-  });
-  // console.log(linkArray);
+  const memeList = [];
   const [chosenMeme, setChosenMeme] = useState('');
-  const link =
-    'https://api.memegen.link/images/buzz/memes/memes_everywhere.gif';
   const [top, setTop] = useState('');
   const [bottom, setBottom] = useState('');
+  const src = `https://api.memegen.link/images/${chosenMeme || 'buzz'}/${
+    top || '_'
+  }/${bottom}.png`;
 
-  // const downloadLink =
-  //   chosenMeme === ''
-  //     ? link
-  //     : `${linkArray[chosenMeme - 1]}/${top === '' ? '_' : top}${
-  //         bottom === '' ? bottom : '/' + bottom
-  //       }.png`;
+  async function componentDidMount() {
+    // GET request using fetch with async/await
+    const response = await fetch('https://api.memegen.link/templates');
+    const data = await response.json();
+    data.map((resp) => {
+      return memeList.push(resp.id);
+    });
+    this.setState({ totalReactPackages: data.total });
+  }
+  const resp = componentDidMount().then((response) =>
+    console.log(response).catch((err) => console.log(err)),
+  );
+  console.log(resp);
+  console.log(memeList);
 
   return (
     <>
@@ -50,7 +32,7 @@ export default function App() {
       <input
         id="top"
         onChange={(event) => {
-          setTop(top === '' ? '__' : event.target.value);
+          setTop(event.target.value);
         }}
       />
       <label htmlFor="bottom">Bottom text:</label>
@@ -65,45 +47,22 @@ export default function App() {
         id="meme"
         onChange={(event) => {
           setChosenMeme(event.target.value);
-          console.log(linkArray[chosenMeme - 5]);
         }}
       />
       <br />
       Download
-      <button
-        onClick={() =>
-          saveAs(
-            chosenMeme === ''
-              ? link
-              : `${linkArray[chosenMeme - 1]}/${top === '' ? '_' : top}${
-                  bottom === '' ? bottom : '/' + bottom
-                }.png`,
-            'meme.jpg',
-          )
-        }
-      >
-        Download
-      </button>
+      <button onClick={() => saveAs(src, 'meme.jpg')}>Download</button>
       <img
         data-test-id="meme-image"
         style={{ width: '400px', height: '400px' }}
         alt="meme"
-        src={
-          chosenMeme.length === 0
-            ? link
-            : containsNumbers(chosenMeme) === false
-            ? `https://api.memegen.link/images/${chosenMeme}/${
-                top === '' ? '_' : top
-              }${bottom === '' ? bottom : '/' + bottom}.png`
-            : `${linkArray[chosenMeme - 1]}/${top === '' ? '_' : top}${
-                bottom === '' ? bottom : '/' + bottom
-              }.png`
-        }
+        src={`https://api.memegen.link/images/${chosenMeme || 'buzz'}/${
+          top || '_'
+        }/${bottom}.png`}
       />
       <br /> <br /> <br /> <br /> <br /> <br />
       memes you can choose from: <br />
       {memeList}
-      {/* data-test-id="meme-image" */}
     </>
   );
 }
